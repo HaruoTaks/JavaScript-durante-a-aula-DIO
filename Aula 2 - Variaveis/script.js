faa57ce03748e3611349e256b7ae03eab7d13988
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-02 18:45:28 -0300

    Entendendo variáveis e seus valores

---
+++ Aula 2 - Variaveis/script.js
@@ -0,0 +1,104 @@
+// tipos primitivos
+
+//booleam
+/*var v0uf = false;
+console.log(typeof(v0uf));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'Rodrigo';
+console.log(typeof(nome)); */
+
+// Como declarar variáveis
+// Declaração com var
+/*var variavel = 'Rodrigo';
+variavel = 'Haruo';
+console.log(variavel);*/
+
+//Declaração com let
+/*let variavel2 = 'Rodrigo';
+variavel2 = 'Haruo';
+console.log(variavel2);*/
+
+//Declaração com const(Váriavel não pode ser reatribuída)
+/*const constante = 'Rodrigo';
+console.log(constante);*/
+
+// Escopo 
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();*/
+
+//Atribuiçao
+// var atribuicao  = 'Rodrigo';
+
+//Comparação
+// var comparacao = '0' == 0;
+// console.log(comparacao);
+
+// var comparacaoIdentica = '0' === 0;
+// console.log(comparacaoIdentica);
+
+//Adição
+// var adicao = 1+1;
+// console.log(adicao);
+
+// //Subtração
+// var subtracao = 2-1;
+// console.log(subtracao);
+
+// //Multiplicação
+// var multiplicacao = 2*3;
+// console.log(multiplicacao);
+
+// //Divisão real
+// var divisaoReal = 6/2;
+// console.log(divisaoReal);
+
+// //Divisão inteira
+// var divisaoInteira = 5%2;
+// console.log(divisaoInteira);
+
+// //Potênciação
+// var potenciacao = 2**10;
+// console.log(potenciacao);
+
+//Operadores relacionais
+// //Maior que
+// var maiorQue = 5>2;
+// console.log(maiorQue);
+
+// //Menor que
+// var menorQue = 5<2;
+// console.log(menorQue);
+
+// //Maior ou igual
+// var maiorOuIgual = 5>=2;
+// console.log(maiorOuIgual);
+
+// //Menor ou igual
+// var menorOuIgual = 5<=2;
+// console.log(menorOuIgual);
+
+//Operadores Lógicos
+
+//Operador e(&&)
+var e = true && true;
+console.log(e);
+
+//Operador ou(||)
+var ou = true || false;
+console.log(ou);
+
+//Operador não(!)
+var nao = !true;
+console.log(nao);
