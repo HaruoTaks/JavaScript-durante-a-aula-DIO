91cd976c42af96eda4ae0e88ac06cff4401df613
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-05 14:07:55 -0300

    Aprofundando em funções

---
+++ Aula 6 - Aprofundando em funções/script.js
@@ -0,0 +1,32 @@
+// Tipos de Função
+
+// Declarativas
+// function funcao(){
+//     console.log('Sou uma mensagem de função declarativa');
+// }
+
+// funcao();
+
+// Expressão de função
+// Com nomeação
+// var funcao = function funcao(){
+//     console.log('Sou uma mensagem de função de expressão!');
+// }
+
+// funcao();
+
+// Sem nomeação
+// var funcao = function(){
+//     console.log('Sou uma mensagem de função de expressão!');
+// }
+
+// funcao();
+
+// // Arrow function
+// var funcao = () => {
+//     console.log('Sou uma arrow function');
+// }
+
+// funcao()
+
+
